407871a24bf962873a42fa65114ee6e587ca79d0
tgu <stevengu2019@hotmail.com>
2022-04-06 09:29:17 +0200

    srand

--- labyrinthe.cpp
+++ labyrinthe.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 #include <vector>
-<<<<<<< HEAD
 #include <sstream>
 #include <cstdlib>
 #include <iomanip>
 #include <ctime>
 
-=======
-#include <cstdlib>
->>>>>>> origin/main
 
 using namespace std;
 
@@ -46,7 +42,6 @@ int murs (labyrinthe l){
 }
 
 
-<<<<<<< HEAD
 int nbMurLab(labyrinthe &l){
     /*
         nb total de mur pseudo-lab : n(m-1)+m(n-1)
@@ -141,75 +136,10 @@ labyrinthe pseudeGenerer(int m, int n){
         }
     }
     init_enceinte(l);
-=======
-bool estLabyrinthe(labyrinthe l, vector<vector<bool> > &passed){
-    bool V = true;
-
-}
-
-labyrinthe pseudeGenerer(int a, int b){
-    labyrinthe l;
-    for(int i = 0; i < a; i++){
-        for(int j = 0; j < b; j++){
-            if(rand()> RAND_MAX/2){
-                l[i][j].bas == false;
-            }
-            if(rand()> RAND_MAX/2){
-                l[i][j].haut == false;
-            }
-            if(rand()> RAND_MAX/2){
-                l[i][j].gauche == false;
-            }
-            if(rand()> RAND_MAX/2){
-                l[i][j].droite == false;
-            }
-        }
-    }
-    for(int bord = 0; bord < b; bord++){
-        l[0][bord].haut = false;// the top side
-        l[a-1][bord].bas = false;//the bottom side
-    }
-    for(int side = 0; side < a; side++){
-        l[side][0].gauche = false;
-        l[side][b-1].droite = false;
-    }
->>>>>>> origin/main
     return l;
 }
 
 
-<<<<<<< HEAD
-=======
-void dessine(labyrinthe l){
-    stringstream s;
-    for(int len = 0;len < l[i].size()*3 + 1;len++){
-        s<<"*";
-    }
-    s<<endl;
-    for(int i = 0; i < l[i].size();i++){
-        s<<"*";
-        for(int j = 0; j < l[i].size();j++){
-            if(l[i][j].droite == false){
-                s<<"  *";
-            }else if(l[i][j].droite != false){
-                s<<"   ";
-            }
-        }
-        s<<endl;
-        s<<"*";
-        for(int k = 0; k < l[i].size();k++){
-            if(l[i][k].droite == false){
-                s<<"***";
-            }else if(l[i][k].droite != false){
-                s<<"  *";
-            }
-        }
-        s<<endl;
-    }
-    cout<<s<<endl;
-}
-
->>>>>>> origin/main
 void parcourir(labyrinthe l, int i, int j){
     l[i][j].chemin = true;
     if (l[i][j].bas == true && l[i + 1][j].chemin == false)
